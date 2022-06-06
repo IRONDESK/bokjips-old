011962c941c5dae7bfec5a3966ba1e2d253b854c
SOOCHUL SON <zerozerobase@gmail.com>
2022-06-07 00:19:35 +0900

    :sparkles: 회원가입 유효성 기능 구현

--- src/pages/user/join.jsx
+++ src/pages/user/join.jsx
@@ -1,30 +1,92 @@
 import React, { useState } from 'react'
+import axios from 'axios'
 
 import styled from '@emotion/styled'
 import { COLOR } from '../../constants'
 
 export default function join() {
+  const [userEmail, setUserEmail] = useState()
+  const [userName, setUserName] = useState()
+  const [userPwd, setUserPwd] = useState()
+
+  const EmailRegex =
+    /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
+  const PwdRegex = /(?=.*[~!@#$%^&*])[~!@#$%^&*]/
+  // axios.post('http://52.79.165.66:8081/user/insert', )
+
+  const EmailAutoComplete = e => {
+    setUserEmail(userEmail + e.target.innerHTML)
+  }
   return (
     <>
       <Container>
         <Form>
-          <Label for="user-email">
+          <Label htmlFor="user-email">
             이메일
-            <Input id="user-email" />
-            {/* <Alert>이메일 형식이 올바르지 않습니다.</Alert> */}
+            <Input
+              id="user-email"
+              type="text"
+              onChange={e => {
+                setUserEmail(e.target.value)
+              }}
+              value={userEmail}
+            />
+            {EmailRegex.test(userEmail) ||
+            !userEmail ||
+            userEmail.charAt(userEmail.length - 2) !== '@' ? null : (
+              <Alert>이메일 형식이 올바르지 않습니다.</Alert>
+            )}
+            {userEmail && userEmail.charAt(userEmail.length - 1) == '@' ? (
+              <Domains onClick={EmailAutoComplete}>
+                <Domain>gmail.com</Domain>
+                <Domain>naver.com</Domain>
+              </Domains>
+            ) : null}
           </Label>
-          <Label for="user-name">
+          <Label htmlFor="user-name">
             이름
-            <Input id="user-name" />
+            <Input
+              id="user-name"
+              onChange={e => {
+                setUserName(e.target.value)
+              }}
+              value={userName}
+            />
             {/* <Alert>이미 존재하는 이름입니다.</Alert> */}
           </Label>
-          <Label for="user-pw">
+          <Label htmlFor="user-pw">
             비밀번호
-            <Input type="password" id="user-pw" placeholder="최소 6자 이상" />
-            {/* <Alert>비밀번호 형식이 올바르지 않습니다.</Alert> */}
+            <Input
+              type="password"
+              id="user-pw"
+              onChange={e => {
+                setUserPwd(e.target.value)
+              }}
+              value={userPwd}
+            />
+            <PwdCheck>
+              <Item check={userPwd && userPwd.length > 5}>
+                <i className="material-icons">check</i>6자 이상
+              </Item>
+              <Item check={userPwd && PwdRegex.test(userPwd)}>
+                <i className="material-icons">check</i>한 개 이상의 특수문자
+                포함 (~!@#$%^&*)
+              </Item>
+            </PwdCheck>
           </Label>
-          <Button type="submit" className="material-icons">
-            check
+          <Button
+            type="submit"
+            className="material-icons"
+            disabled={
+              userPwd &&
+              userPwd.length > 5 &&
+              PwdRegex.test(userPwd) &&
+              EmailRegex.test(userEmail)
+                ? false
+                : true
+            }
+          >
+            subdirectory_arrow_right
           </Button>
         </Form>
       </Container>
@@ -42,6 +104,7 @@ const Form = styled.form`
   top: 50%;
   left: 50%;
   width: 350px;
+  height: 450px;
   transform: translate(-50%, -50%);
   box-shadow: 0 0 15px 3px rgba(0, 0, 0, 0.1);
   border-radius: 10px;
@@ -70,12 +133,51 @@ const Input = styled.input`
 `
 const Alert = styled.span`
   display: block;
-  margin: 5px 3px;
+  margin: 7px 0 0 3px;
   color: ${COLOR.report};
   font-size: 15px;
   font-weight: 400;
 `
+const Domains = styled.ul`
+  display: flex;
+  margin-top: 7px;
+  flex-wrap: wrap;
+  gap: 6px 5px;
+`
+const Domain = styled.li`
+  cursor: pointer;
+  padding: 3px 8px;
+  color: ${COLOR.darkgray};
+  font-weight: 500;
+  text-align: center;
+  border: 1px solid ${COLOR.darkgray};
+  border-radius: 5px;
+  &:hover {
+    background-color: ${COLOR.darkgray};
+    color: #fff;
+  }
+`
+const PwdCheck = styled.ul`
+  margin-top: 7px;
+`
+const Item = styled.li`
+  display: flex;
+  margin-top: 5px;
+  align-items: center;
+  color: ${prop => (prop.check ? COLOR.check : COLOR.darkgray)};
+  font-size: 14px;
+  font-weight: 400;
+  i {
+    padding: 1.5px;
+    margin-right: 3px;
+    font-size: 13px;
+    background-color: ${prop => (prop.check ? COLOR.check : 'none')};
+    color: ${prop => (prop.check ? '#fff' : 'none')};
+    border-radius: 100%;
+  }
+`
 const Button = styled.button`
+  cursor: ${prop => (prop.disabled ? 'not-allowed' : 'pointer')};
   display: block;
   margin: 0 auto;
   width: 55px;
