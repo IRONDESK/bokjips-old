7276b8d1710b3c99447b39e27bc35d3f9efc6ee6
IRONDESK <irondesk@IRONDESKui-MacBookPro.local>
2022-07-10 22:16:52 +0900

    :lipstick: 필터 기능 인터페이스 구현

---
+++ src/lib/GlobalData.jsx
@@ -0,0 +1,13 @@
+export const MainFilter = [
+  { id: 1, value: "workinhome", text: "🏠 재택근무" },
+  { id: 2, value: "kindergarten", text: "👨‍👩‍👧 사내어린이집" },
+  { id: 3, value: "nonintegratedwage", text: "💰 비포괄임금" },
+  { id: 4, value: "unlimitedvacation", text: "🏖 무제한연차" },
+  { id: 5, value: "freelunch", text: "🍚 점심은무료" },
+  { id: 6, value: "stockoption", text: "📈 스톡옵션" },
+  { id: 7, value: "physical", text: "🏋🏼 체력단련지원" },
+  { id: 8, value: "daddybaby", text: "🙆🏻 아빠도육아휴직" },
+  { id: 9, value: "birthday", text: "🎂 생일선물" },
+  { id: 10, value: "loan", text: "💸 대출이자지원" },
+  { id: 11, value: "house", text: "🏘 사택제공" },
+];
