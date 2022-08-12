7915b073efbf621e2de9e20d3f1670add84d788b
IRONDESK <zerozerobase@gmail.com>
2022-08-12 20:12:02 +0900

    :recycle: 작성자 본인만 댓글 삭제 가능하도록 변경

--- src/components/company/Comments.jsx
+++ src/components/company/Comments.jsx
@@ -45,12 +45,15 @@ export default function Comments() {
       });
   };
 
-  const deleteComment = (comments_id) => {
+  const deleteComment = (comments_id, user_id) => {
     const confirmDel = confirm("선택하신 댓글을 삭제하시겠습니까?");
-    if (confirmDel) {
+    if (confirmDel && user_id === userInfo?.logged.user_id) {
       axios
         .delete(`http://52.79.165.66:8081/comments/delete/${comments_id}`)
         .then(() => mutate(getCommentURL));
+    }
+    if (confirmDel && user_id !== userInfo?.logged.user_id) {
+      alert("삭제 권한이 없습니다.");
     }
   };
 
@@ -95,7 +98,7 @@ export default function Comments() {
                     <button
                       className='material-icons'
                       onClick={() => {
-                        deleteComment(el.comments_id);
+                        deleteComment(el.comments_id, el.user_id);
                       }}
                     >
                       delete_forever
